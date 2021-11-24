0b26b3f9027937770212bc2f88a49c069ef4db6e
Matej-Podany <matej@podany.eu>
2021-11-24 14:46:37 +0100

    Display almost done

--- Labs/Project/rele_project/rele_project/main.c
+++ Labs/Project/rele_project/rele_project/main.c
@@ -1,10 +1,10 @@
 /***********************************************************************
- * 
- * Stopwatch with LCD display output.
+ * Water tank project
+ * LCD display controller
  * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
  *
- * Copyright (c) 2017-Present Tomas Fryza
- * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * Copyright (c) 2021-Present Matej Podany
+ * Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
  * 
  **********************************************************************/
@@ -19,7 +19,7 @@
 /* Variables ---------------------------------------------------------*/
 // Custom character definition using https://omerk.github.io/lcdchargen/
 uint8_t customChar[40] = {
-	0b10001,
+	0b10001,  //full tank
 	0b11111,
 	0b11111,
 	0b11111,
@@ -27,23 +27,23 @@ uint8_t customChar[40] = {
 	0b11111,
 	0b11111,
 	0b11111,
+	0b10001,  // almost full tank
+	0b10001,
+	0b10001,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b11111,
+	0b10001,  // almost empty tank
+	0b10001,
 	0b10001,
 	0b10001,
 	0b10001,
 	0b11111,
 	0b11111,
 	0b11111,
-	0b11111,
-	0b11111,
-	0b10001,
-	0b10001,
-	0b10001,
-	0b10001,
-	0b10001,
-	0b11111,
-	0b11111,
-	0b11111,
-	0b10001,
+	0b10001,  // empty tank
 	0b10001,
 	0b10001,
 	0b10001,
@@ -51,7 +51,7 @@ uint8_t customChar[40] = {
 	0b10001,
 	0b10001,
 	0b11111,
-	0b01110,
+	0b01110,  // rainning symbol
 	0b11111,
 	0b01110,
 	0b00000,
@@ -72,7 +72,7 @@ int main(void)
     // Initialize LCD display
     lcd_init(LCD_DISP_ON);
 
-    // Put string(s) at LCD display
+    // Put string at LCD display
     lcd_gotoxy(0, 1);
     lcd_puts("LVL:");
 
@@ -86,11 +86,9 @@ int main(void)
 	// Set DDRAM address
 	lcd_command(1 << LCD_DDRAM);
 	
-	// Display first custom character
-	
-    // Configure 8-bit Timer/Counter2 for Stopwatch
-    // Set the overflow prescaler to 16 ms and enable interrupt
-	TIM0_overflow_4ms();
+    // Configure 8-bit Timer/Counter0 for lcd updates
+    // Set the overflow prescaler to 262 ms and enable interrupt
+	TIM0_overflow_262ms();
 	TIM0_overflow_interrupt_enable();
 
     // Enables interrupts by setting the global interrupt mask
@@ -109,9 +107,9 @@ int main(void)
 
 /* Interrupt service routines ----------------------------------------*/
 /**********************************************************************
- * Function: Timer/Counter2 overflow interrupt
- * Purpose:  Update the stopwatch on LCD display every sixth overflow,
- *           ie approximately every 100 ms (6 x 16 ms = 100 ms).
+ * Function: Timer/Counter0 overflow interrupt
+ * Purpose:  Update LCD display every overflow.
+ *           
  **********************************************************************/
 ISR(TIMER0_OVF_vect)
 {
